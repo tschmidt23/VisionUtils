0d4d17557fa00c095dd57bc02f792f6accbe6aa4
Tanner Schmidt <tws10@cs.washington.edu>
2018-08-06 23:06:57 -0700

    ditched a warning

--- Src/ColorHelpers.cpp
+++ Src/ColorHelpers.cpp
@@ -29,7 +29,9 @@ std::vector<Vec3<unsigned char> > GetColorPalette(const int K) {
 
         std::vector<Color> palette(K);
         for (int k = 0; k < K; ++k) {
-            palette[k] = { rand() % 255, rand() % 255, rand() % 255 };
+            palette[k] = { static_cast<unsigned char>(rand() % 255),
+                           static_cast<unsigned char>(rand() % 255),
+                           static_cast<unsigned char>(rand() % 255) };
         }
 
         return palette;
