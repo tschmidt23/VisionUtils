8fe55c4320a3fd9928e0ddecf97538bb95b7e1a4
Tanner Schmidt <tws10@cs.washington.edu>
2018-09-02 15:05:22 -0700

    KdMatrix

--- Include/vu/KdHelpers.h
+++ Include/vu/KdHelpers.h
@@ -304,4 +304,43 @@ private:
 template <typename Scalar, int C = 0, int X = 1, int Y = 2>
 using KdFeatureMapTree = nanoflann::KDTreeSingleIndexAdaptor<nanoflann::L2_Simple_Adaptor<Scalar, KdFeatureMap<Scalar, C, X, Y> >, KdFeatureMap<Scalar, C, X, Y>, -1, int>;
 
+
+struct KdMatrix {
+public:
+
+    KdMatrix(const NDT::Tensor2f & matrix)
+            : matrix_(matrix) { }
+
+    // Must return the number of data points
+    inline size_t kdtree_get_point_count() const {
+        return matrix_.DimensionSize(1);
+    }
+
+    // Returns the distance between the vector "p1[0:size-1]" and the data point with index "idx_p2" stored in the class:
+    inline float kdtree_distance(const float * p1, const size_t idx_p2,size_t /*size*/) const {
+        return (Eigen::Map<const Eigen::VectorXf>(p1, matrix_.DimensionSize(0)) -
+                Eigen::Map<const Eigen::VectorXf>(&matrix_(0, idx_p2), matrix_.DimensionSize(0))).squaredNorm();
+    }
+
+    // Returns the dim'th component of the idx'th point in the class:
+    // Since this is inlined and the "dim" argument is typically an immediate value, the
+    //  "if/else's" are actually solved at compile time.
+    inline float kdtree_get_pt(const size_t idx, int dim) const {
+        return matrix_(dim, idx);
+    }
+
+    // Optional bounding-box computation: return false to default to a standard bbox computation loop.
+    //   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it again.
+    //   Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
+    template <class BBOX>
+    bool kdtree_get_bbox(BBOX & /*bb*/) const { return false; }
+
+private:
+
+    NDT::ConstTensor2f matrix_;
+
+};
+
+using KdMatrixTree = nanoflann::KDTreeSingleIndexAdaptor<nanoflann::L2_Simple_Adaptor<float, KdMatrix>, KdMatrix, -1, int>;
+
 } // namespace vu
